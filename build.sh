fe87e477ad31725480aeb03777a631bea7a70f10
Ming Chen <ming.c@live.com>
2014-11-11 09:22:17 -0800

    Share schema to solve following error:
      xcodebuild: error: The project 'protobuf-ios' does not contain a scheme named 'protobuf-ios'.
    
    Ensure the schemes of your target are Shared. This option makes a scheme visible to anyone using that project. To enable it, go to the menu: Product > Scheme > Manage Schemes.
    
    see http://ioscowboy.com/2013/06/09/travis-and-ios-projects.html

--- build.sh
+++ build.sh
@@ -1,3 +1,5 @@
 #!/bin/sh -x
 
 xctool -project runtime/protobuf-ios/protobuf-ios.xcodeproj  -scheme protobuf-ios build
+
+#xctool -project runtime/protobuf-ios/protobuf-ios.xcodeproj  -scheme protobuf-iosTests build test
